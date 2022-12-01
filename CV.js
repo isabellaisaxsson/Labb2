a3a8b857ba5f8cbd0444a347a00a02ab7da80564
isabellaisaxsson <isabella.isaksson2@outlook.com>
2022-12-01 21:55:47 +0100

    gjorde sidan responsiv och lade till effekter

--- CV.js
+++ CV.js
@@ -1,31 +1,7 @@
-let id = null; 
+let fadeInelements = document.getElementById(`fadeInelements`);
 
-const elem = document.getElementById("imgOfme");
-
-let pos = 0;
-
-clearInterval(id);
-
-id = setInterval(frame, 5);
-
-function frame(){
-    if(pos == 70){
-        clearInterval(id);
-    }
-    else{
-        pos++;
-        elem.style.top = pos + "px";
-        elem.style.left = pos + "px";
-    }
-};
+fadeInelements.classList.toggle(`fade`);
 
 function myFunction() {
     window.location.href="portfolio.html";  
-  }
-
-
-
-
-
-
-
+  };
---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+Namn: Isabella Isaksson CV
+
+Beskrivning av uppgiften: 
+
+I denna uppgift skapade jag en hemsida som var mitt CV och mitt portfolio.
+Jag använde mig av JavaScript för att göra en fade-in effekt så att alla element förutom
+bakgrunden på hemsidan fade:ar in. Jag gjorde även knappen klickbar med JavaScript som 
+ska ta användaren till mitt portfolio.
+
+Github Demo länk: 
